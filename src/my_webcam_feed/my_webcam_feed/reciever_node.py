7d87830f68f7faf992cdf1a4d3fd3140b8869dd6
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-02-10 23:14:05 +0530

    added a cascade classifier for face detection

--- src/my_webcam_feed/my_webcam_feed/reciever_node.py
+++ src/my_webcam_feed/my_webcam_feed/reciever_node.py
@@ -12,7 +12,8 @@ class ImageProcessor(Node):
 
         self.img_sub  = self.create_subscription(Image,"no_process_node",self.callback,10)
         self.bridge = CvBridge()
-        
+        self.cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
         self.count = 1
 
     def callback(self,msg:Image):
@@ -20,11 +21,17 @@ class ImageProcessor(Node):
         cv_image = self.bridge.imgmsg_to_cv2(msg)
         # cv2.imwrite(f"src/my_webcam_feed/my_webcam_feed/feed/img{self.count}.jpg",cv_image)
         # self.count = self.count+1
-        cv2.imshow("hola",cv_image)
-
+        gray = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
+        faces = self.cascade.detectMultiScale(gray,scaleFactor=1.3,minNeighbors=5)
         edges = cv2.Canny(image=cv_image, threshold1=100, threshold2=200) 
  
         cv2.imshow('Canny Edge Detection', edges)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        
+        cv2.imshow("hola",cv_image)
+
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return 
